adab30d165d7490261ad7e8d305539e8cce9af42
Lee Russell Mejares <mejareslee@gmail.com>
2026-05-31 15:50:44 +0800

    Fix featured games next arrow, fix sitemap syntax error

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,5 @@
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+@php header('Content-Type: application/xml') @endphp
+<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url><loc>{{ url('/') }}</loc><priority>1.0</priority></url>
     <url><loc>{{ url('/games') }}</loc><priority>0.9</priority></url>
